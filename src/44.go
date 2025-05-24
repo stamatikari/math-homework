82645f91d5cad269ffc54ec47f229244aa6aad2d
stamatikari <stamatikari@gmail.com>
2025-05-24 21:32:38 +0000

    Add 44.go

---
+++ src/44.go
@@ -0,0 +1,14 @@
+def add_numbers(x, y):
+    return x + y
+
+def subtract_numbers(x, y):
+    return x - y
+
+def multiply_numbers(x, y):
+    return x * y
+
+def divide_numbers(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    else:
+        return x / y
